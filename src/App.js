24316d34c1d7717d1c74b5931a6bff308aa2cf9a
Mateo mojica <mmojicah.84@gmail.com>
2022-05-22 20:22:22 -0500

    useState hook with object as a state

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState({
+    num: 0,
+    id: "abcd"
+  });
   
   const handleAdd = () => {
-    setCount((prev) => prev+1);
-    setCount((prev) => prev+1);
+    setCount((prev) => {
+      return(
+        {...prev, num: prev.num + 1}
+      )
+    });
     console.log(count);
   }
 
   const handleSubstract = () => {
-    setCount((prev) => prev-1);
+    setCount((prev) => {
+      return(
+        {...prev, num: prev.num - 1}
+      )
+    });
     console.log(count);
   }
 
   return (
     <div className="App">
       <button onClick={handleSubstract}>-</button>
-      <span>{count}</span>
+      <span>{count.num}</span>
       <button onClick={handleAdd}>+</button>
     </div>
   );
